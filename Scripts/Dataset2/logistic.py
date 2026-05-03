4792cc7f0870e17a6d47deae70fa83435836b356
andrelewis-blip <andrevikashlewis@gmail.com>
2026-05-03 18:17:05 -0400

    Refactor logistic regression to use a pipeline for scaling

--- Scripts/Dataset2/logistic.py
+++ Scripts/Dataset2/logistic.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 df = pd.read_csv("data/Data/cleaned.csv")
 
@@ -11,7 +13,9 @@ y = df['Default']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42,stratify=y)
 
-model = LogisticRegression(max_iter=1000)
+model = Pipeline([('scaler', StandardScaler()),
+    ('lr', LogisticRegression(max_iter=1000))
+])
 
 skf = StratifiedKFold(
     n_splits=5,
@@ -30,3 +34,4 @@ y_pred = model.predict(X_test)
 
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
+
